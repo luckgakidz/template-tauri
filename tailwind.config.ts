b42d896bf425f2ec2a5efccee14c0395efccd899
luckgakidz <luck.ga.kidz@gmail.com>
2023-12-09 21:31:21 +0900

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+  ],
+  theme: {
+    extend: {},
+  },
+};
+export default config;
